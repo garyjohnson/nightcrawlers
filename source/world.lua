f3ad1501f793e53caf93c63eccc2a36b3602aa53
Gary Johnson <gary@gjtt.com>
2022-03-09 20:43:03 -0600

    Refactoring World

--- source/world.lua
+++ source/world.lua
@@ -24,16 +24,3 @@ function World:init()
   self:addEntity(self.person, 3)
   self:addEntity(self.water, 4)
 end
-
-function World:update(dt)
-  World.super.update(self, dt)
-end
-
-function World:onKeyReleased(key)
-  World.super.onKeyReleased(self, key)
-
-  if key == "r" then
-    self.terrain:generate()
-    self.person.y = 1
-  end
-end
